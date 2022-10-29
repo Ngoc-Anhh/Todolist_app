e730bd4d6d12fd9c2cd7b2f604d8c536c22c71f6
Ngoc-Anhh <94812338+Ngoc-Anhh@users.noreply.github.com>
2022-10-30 01:06:46 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
     return (
         <div style={{ textAlign: 'left' }}>
-            <h3>Made by 4501104098 - LE THI THANH HUYEN</h3>
+            <h3>Made by 4501104012 - HỒ THỊ NGỌC ÁNH</h3>
             <h3>
                 Github: <a href="https://github.com/LETHITHANHHUYEN/todo_app">here</a>
             </h3>
