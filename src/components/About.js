b9b4e8f6839c6529922aaf05e5c8b1c912c8d030
Ngoc-Anhh <94812338+Ngoc-Anhh@users.noreply.github.com>
2022-10-30 01:09:13 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
         <div style={{ textAlign: 'left' }}>
             <h3>Made by 4501104012 - HỒ THỊ NGỌC ÁNH</h3>
             <h3>
-                Github: <a href="https://github.com/LETHITHANHHUYEN/todo_app">here</a>
+                Github: <a href="https://github.com/Ngoc-Anhh/Todolist_app">here</a>
             </h3>
         </div>
     )
